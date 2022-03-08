1afc798181ceb7d6d719e570b43880a92ae88ee6
Srikumar Sastry <srikumarsastry1997@gmail.com>
2022-03-09 00:50:02 +0100

    Update data_preprocess.py

--- data_preprocess.py
+++ data_preprocess.py
@@ -6,28 +6,28 @@ from tensorflow.keras.utils import to_categorical
 from tqdm import tqdm
 
 
-def read_images(path='UCMerced_LandUse/Images/', shape=(256, 256, 3)):
-    x_train = np.zeros((0, shape[0], shape[1], shape[2]), dtype=np.float32)
-    y_train = np.zeros((0, 1), dtype=np.uint8)
-    x_test = np.zeros((0, shape[0], shape[1], shape[2]), dtype=np.float32)
-    y_test = np.zeros((0, 1), dtype=np.uint8)
+def read_images(path='UCMerced_LandUse/Images/', shape=(64, 64, 3)):
+    xtr, ytr, xt, yt = [], [], [], [] 
     dirs = os.listdir(path)
     label = 0
     for i in tqdm(dirs):
         count = 0
-        for pic in glob.glob(path+i+'/*.tif'):
+        imgs = glob.glob(path+i+'/*.jpg')
+        idx = np.split(imgs, [3*len(imgs)//4])
+        for pic in idx[0]:
             im = Image.open(pic)
             im = np.array(im)
-            if((im.shape[0] == 256) and (im.shape[1] == 256) and count < 90):
-                if(count < 5):
-                    x_test = np.concatenate((x_test, im.reshape((1,)+shape)), axis=0)
-                    y_test = np.concatenate((y_test, [[label]]), axis=0)
-                else:
-                    x_train = np.concatenate((x_train, im.reshape((1,)+shape)), axis=0)
-                    y_train = np.concatenate((y_train, [[label]]), axis=0)
-                count = count + 1
+            if((im.shape[0] == shape[0]) and (im.shape[1] == shape[1])):
+                xtr.append(im)
+                ytr.append([label])
+        for pic in idx[1]:
+            im = Image.open(pic)
+            im = np.array(im)
+            if((im.shape[0] == shape[0]) and (im.shape[1] == shape[1])):
+                xt.append(im)
+                yt.append([label])
         label = label + 1
-    return np.array(x_train), np.array(x_test), np.array(y_train), np.array(y_test)
+    return np.array(xtr).astype(np.float32), np.array(xt).astype(np.float32), np.array(ytr).astype(np.uint8), np.array(yt).astype(np.uint8)
 
 
 def preprocess(x_train, x_test, y_train, y_test, factor, n_classes):
@@ -43,11 +43,12 @@ def preprocess(x_train, x_test, y_train, y_test, factor, n_classes):
 
 if __name__ == '__main__':
 
-    path = 'UCMerced_LandUse/Images/'
+    path = 'EuroSat/2750/'
     factor = 255
-    n_classes = 21
+    n_classes = 10
+    img_shape = (64, 64, 3)
 
     x_train, x_test, y_train, y_test = read_images(path)
     x_train, x_test, y_train, y_test = preprocess(x_train, x_test, y_train, y_test, factor, n_classes)
 
-    np.savez_compressed("uc_merced", x_train=x_train, x_test=x_test, y_train=y_train, y_test=y_test)
+    np.savez_compressed("eurosat", x_train=x_train, x_test=x_test, y_train=y_train, y_test=y_test)
