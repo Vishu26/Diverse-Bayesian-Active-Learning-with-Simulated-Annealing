54d1ce2752f3afea70d32a3ac5cd4ab5f85f86ba
Srikumar Sastry <srikumarsastry1997@gmail.com>
2022-04-19 21:10:25 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dense, Flatten, Dropout, Convolution2D, MaxPooling2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D, Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import categorical_crossentropy
+import tensorflow as tf
+from tensorflow.keras.initializers import glorot_uniform
 
-def LeNet(input_shape):
+def LeNet(input_shape, n_classes=10):
     model = Sequential()
 
     model.add(Conv2D(filters=6, kernel_size=(5, 5), padding='same',
@@ -15,11 +17,137 @@ def LeNet(input_shape):
     model.add(Flatten())
     model.add(Dense(120, activation='relu', name='embedding'))
     model.add(Dropout(0.55))
-    model.add(Dense(10, activation='softmax'))
+    model.add(Dense(n_classes, activation='softmax'))
 
     opt = Adam(learning_rate=0.001)
-    model.compile(loss=categorical_crossentropy, 
-                optimizer=opt, 
-                metrics=['accuracy']) 
+    model.compile(loss=categorical_crossentropy,
+                optimizer=opt,
+                metrics=['accuracy'])
+
+    return model
+
+def VGG(input_shape, n_classes=10):
+    vgg = tf.keras.applications.VGG16(include_top=False, input_shape=input_shape)
+    vgg.trainable = False
+    #global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+    flat = tf.keras.layers.Flatten(name='embedding')
+    prediction_layer = tf.keras.layers.Dense(n_classes,activation='softmax')
+    model = tf.keras.Sequential([vgg, flat, prediction_layer])
+
+    opt = Adam(learning_rate=0.001)
+    model.compile(loss=categorical_crossentropy,
+                optimizer=opt,
+                metrics=['accuracy'])
+
+    return model
+
+def identity_block(X, f, filters, stage, block):
+
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    F1, F2, F3 = filters
+
+    X_shortcut = X
+
+    X = Conv2D(filters=F1, kernel_size=(1,1), strides=(1,1), padding='valid', name=conv_name_base+'2a', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base+'2a')(X)
+    X = Activation('relu')(X)
+
+    X = Conv2D(filters=F2, kernel_size=(f,f), strides=(1,1), padding='same', name=conv_name_base+'2b', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base+'2b')(X)
+    X = Activation('relu')(X)
+
+    X = Conv2D(filters=F3, kernel_size=(1,1), strides=(1,1), padding='valid', name=conv_name_base+'2c', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base+'2c')(X)
+
+    X = Add()([X, X_shortcut])
+    X = Activation('relu')(X)
+
+    return X
+
+def convolutional_block(X, f, filters, stage, block, s=2):
+
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    F1, F2, F3 = filters
+
+    X_shortcut = X
+
+    X = Conv2D(filters=F1, kernel_size=(1,1), strides=(s,s), name=conv_name_base+'2a', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base+'2a')(X)
+    X = Activation('relu')(X)
+
+    X = Conv2D(filters=F2, kernel_size=(f,f), strides=(1,1), padding='same', name=conv_name_base+'2b', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base+'2b')(X)
+    X = Activation('relu')(X)
+
+    X = Conv2D(filters=F3, kernel_size=(1,1), strides=(1,1), name=conv_name_base+'2c', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base+'2c')(X)
+
+    X_shortcut = Conv2D(F3, kernel_size=(1,1), strides=(s,s), name=conv_name_base+'1', kernel_initializer=glorot_uniform(seed=0))(X_shortcut)
+    X_shortcut = BatchNormalization(axis=3, name=bn_name_base+'1')(X_shortcut)
+
+    X = Add()([X, X_shortcut])
+    X = Activation('relu')(X)
+
+    return X
+
+
+def resNet(nc=10):
+
+    # Define the input as a tensor with shape input_shape
+    X_input = Input((28,28,1))
+
+    # Zero-Padding
+    X = ZeroPadding2D((3, 3))(X_input)
+
+    # Stage 1
+    X = Conv2D(64, (7, 7), strides = (2, 2), name = 'conv1',)(X)
+    X = BatchNormalization(axis = 3, name = 'bn_conv1')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((3, 3), strides=(2, 2))(X)
+
+    # Stage 2
+    X = convolutional_block(X, f = 3, filters = [64, 64, 256], stage = 2, block='a', s = 1)
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='b')
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='c')
+
+    # Stage 3
+    X = convolutional_block(X, f = 3, filters = [128, 128, 512], stage = 3, block='a', s = 2)
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='b')
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='c')
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='d')
+
+    # Stage 4
+    X = convolutional_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block='a', s = 2)
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='b')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='c')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='d')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='e')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='f')
+
+    # Stage 5
+    X = convolutional_block(X, f = 3, filters = [512, 512, 2048], stage = 5, block='a', s = 2)
+    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='b')
+    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='c')
+
+    # AVGPOOL
+    # X = AveragePooling2D((2,2), name='avg_pool')(X)
+
+    # output layer
+    X = Flatten()(X)
+    X = Dense(nc, activation='softmax', name='fc' + str(nc))(X)
+
+    # Create model
+    model = Model(inputs = X_input, outputs = X, name='ResNet50')
+
+    #Compile the model
+    #model.compile(optimizer=optimizer, loss=objective, metrics=['accuracy'])
+    opt = Adam(learning_rate=0.001)
+    model.compile(loss=categorical_crossentropy,
+                optimizer=opt,
+                metrics=['accuracy'])
 
     return model
